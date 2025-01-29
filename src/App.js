09da507eba4a6390b8958c54ee337b1f3b511c4c
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-28 20:41:46 -0500

    add form button, click handler

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.png';
 import './App.css';
 
 function App() {
+  const clickHandler = (evt) => {
+    evt.preventDefault();
+    console.log("Form submitted.");
+    
+  };
+
   return (
     <div className="App">
       Building A Basic Form
-      <form className="centered-form" action="https://server.com/action_page">
+      <form className="centered-form">
+      
         <label>
           First Name: 
           <input type="text" style={{ fontWeight: 'bold' }} />
         </label>
+        <button onClick={clickHandler}>
+          Submit Form Data 
+        </button>
       </form>
     </div>
   );
